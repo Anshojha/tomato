ba7dd06c88639bd78b5b6f78264a9446e87bfbcc
Anshojha <anshojha24@gmail.com>
2024-11-22 16:29:45 +0530

    Order update feature added

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 
 const placeOrder = async(req , res) => {
-    const frontend_url = "http://localhost:5173"
+    const frontend_url = "http://localhost:5174"
     try {
         const newOrder = new orderModel({
             userId : req.body.userId,
@@ -95,4 +95,14 @@ const listOrders =async  (req, res) => {
     
 }
 
-export {placeOrder, verify, userOrders, listOrders}
+const updateStatus = async (req, res) => {
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId, {status : req.body.status})
+        res.json({success : true , message : "Status updated"})
+    } catch (error) {
+        console.log(error)
+        res.json({success : false , message : "Error in the upstadestatus"})
+    }
+} 
+
+export {placeOrder, verify, userOrders, listOrders, updateStatus}
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from "express"
 import authMiddleware from "../middleware/auth.js"
-import { placeOrder, verify, userOrders, listOrders } from "../controller/orderController.js";
+import { placeOrder, verify, userOrders, listOrders, updateStatus } from "../controller/orderController.js";
 
 
 const orderRouter = express.Router();
@@ -9,5 +9,6 @@ orderRouter.post("/place",authMiddleware, placeOrder);
 orderRouter.post("/verify", verify)
 orderRouter.post("/userorders", authMiddleware, userOrders)
 orderRouter.get("/list", listOrders)
+orderRouter.post("/status", updateStatus)
 
 export default orderRouter;
